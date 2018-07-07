2af73943e5a0cb262e3c4c406cfcebe4559ed7b5
Igor Franca <igor.filho@ccc.ufcg.edu.br>
2018-07-07 16:02:41 -0300

    Item e filhas 1.0

--- src/projeto/Item.java
+++ src/projeto/Item.java
@@ -42,4 +42,30 @@ public abstract class Item {
     }
     @Override
     public abstract String toString();
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((idNumerico == null) ? 0 : idNumerico.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		if (idNumerico == null) {
+			if (other.idNumerico != null)
+				return false;
+		} else if (!idNumerico.equals(other.idNumerico))
+			return false;
+		return true;
+	}
+    
 }
