f1ed427a75e8c53f1c93068e78027ea7ac7c4239
victorpazb <40031882+victorpazb@users.noreply.github.com>
2018-07-10 23:10:54 -0300

    Create Facade.java

---
+++ src/projeto/Facade.java
@@ -0,0 +1,31 @@
+public class Facade {
+
+	ControllerItens novoController = new ControllerItens();
+
+	public String adicionaItemPorQtd(String nome, String categoria, int qtd, String unidadeDeMedida,
+			String localDeCompra, double preco) {
+		return novoController.adicionaItemPorQtd(nome, categoria, qtd, unidadeDeMedida, localDeCompra, preco);
+	}
+
+	public String adionaItemPorQuilo(String nome, String categoria, double kg, String localDeCompra, double preco) {
+		return novoController.adionaItemPorQuilo(nome, categoria, kg, localDeCompra, preco);
+	}
+
+	public String adionaItemPorUnidade(String nome, String categoria, int unidade, String localDeCompra, double preco) {
+		return novoController.adionaItemPorUnidade(nome, categoria, unidade, localDeCompra, preco);
+	}
+
+	public void atualizaItem(int idNumerico, String atributo, String novoValor) {
+		novoController.atualizaItem(idNumerico, atributo, novoValor);
+
+	}
+
+	public void adicionaPrecoItem(int idNumerico, String localDeCompra, double preco) {
+		novoController.adicionaPrecoItem(idNumerico, localDeCompra, preco);
+	}
+
+	public void deletaItem(String idNumerico) {
+		novoController.deletaItem(idNumerico);
+	}
+
+}
