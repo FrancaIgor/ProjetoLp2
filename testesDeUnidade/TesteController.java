9a80534f69e5ad21c55ac56c959edfa1bd7cc1a0
Igor Franca <igor.filho@ccc.ufcg.edu.br>
2018-07-14 16:09:17 -0300

    testes de unidade - controller e itens

---
+++ testesDeUnidade/TesteController.java
@@ -0,0 +1,58 @@
+
+import org.junit.Assert.*;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import projeto.ControllerItens;
+import projeto.Item;
+import projeto.ItemPorQuantidade;
+import projeto.ItemPorQuilo;
+import projeto.ItemPorUnidade;
+
+public class TesteController {
+
+	private ControllerItens controllerItem = new ControllerItens();
+	
+	@Before
+	public void CriaItem() {
+		Item itemPorQtd = new ItemPorQuantidade("Pasta de dente", "higiene pessoal","Baratão", "ml",300,2.00);
+		Item itemPorQuilo = new ItemPorQuilo("batata","alimentos nao industrializados", "Baratao", 1.50,1.2);
+		Item itemPorUnidade = new ItemPorUnidade("Queijo minas Dali", "alimentos industrializados","Baratão",2,2.0);
+	}
+	
+	@Test
+	public void testadicionaItemPorQtd() {
+		assertEquals("CADASTRO REALIZADO",controllerItem.adicionaItemPorQtd("salgadinho", "produto industrializado", 3, "kg", "rede Lascados", 2.50));
+		
+	}
+	
+	@Test
+	public void testItemPorQuilo() {
+		assertEquals("CADASTRO REALIZADO", controllerItem.adionaItemPorQuilo("batata", "produto não industrializado", 2.5, "feira da prata", 3.50));
+	}
+	
+	@Test
+	public void testItemPorUnidade() {
+		assertEquals("CADASTRO REALIZADO", controllerItem.adionaItemPorUnidade("ovo", "não industralizados", 3, "mercado do seu zé", 0.75));
+	}
+	
+	@Test
+	public void testAtualizaItem() {
+		controllerItem.atualizaItem(1, "nome","batata doce");
+		
+	}
+	
+	@Test
+	public void testadicionaPrecoItem() {
+		controllerItem.adicionaPrecoItem(1, "rede lascados", 2.50);
+	}
+	
+	@Test 
+	public void testDeletaItem() {
+		controllerItem.deletaItem("1");
+	}
+
+}
---
+++ testesDeUnidade/TesteItem.java
@@ -0,0 +1,70 @@
+
+import org.junit.Assert.*;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import projeto.Item;
+import projeto.ItemPorQuantidade;
+import projeto.ItemPorQuilo;
+import projeto.ItemPorUnidade;
+
+public class TesteItem {
+
+	private Item itemQuilo;
+	private Item novoItemQuilo;
+	private Item itemPorQtd;
+	private Item itemPorUnidade;
+	
+	@Before
+	public void CriaItemPorQuilo() {
+		ItemPorQuilo item = new ItemPorQuilo("batata","alimentos nao industrializados", "Baratao", 1.50,1.2);
+		ItemPorQuilo novoitem = new ItemPorQuilo("laranja","alimentos nao industrializados", "Baratao",2.30,2.0);
+	}
+	
+	@Test
+	public void testExistenciaItemPorQuilo() {
+		assertNotNull(itemQuilo);
+		
+	}
+	@Test
+	public void testeCalculaPreco() {
+		assertEquals(1.50*1.2, itemQuilo.calculaPreco());
+	}
+	
+	
+	@Test
+	public void testidNumerico() {
+		assertEquals(1, itemQuilo.getIdNumerico());
+	}
+	
+	@Test
+	public void testEqualsItem() {
+		assertNotEquals(itemQuilo.toString(), novoItemQuilo.toString());
+	}
+	
+	@Test
+	public void testCriaItemPorQtd() {
+		ItemPorQuantidade itemPorQtd = new ItemPorQuantidade("Pasta de dente", "higiene pessoal","Baratão", "ml",300,2.00);
+		
+	}
+	
+	@Test
+	public void testExistenciaItemPorQtd() {
+		assertNotNull(itemPorQtd);
+	}
+	
+	@Test
+	public void testCalculaPrecoItemPorQtd() {
+		assertEquals(2.50, itemPorQtd.calculaPreco());
+	}
+	
+	@Test
+	public void testCriaItemPorUnidade() {
+		ItemPorUnidade ItemPorUnidade = new ItemPorUnidade("Queijo minas Dali", "alimentos industrializados","Baratão",2,2.0); 
+	}
+	
+
+}
